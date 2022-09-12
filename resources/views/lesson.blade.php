9235c2d1be8d392ba062b55b44822a456ea21086
hayategari <hayatesato1@gmail.com>
2022-09-12 10:25:33 +0900

    2

---
+++ resources/views/lesson.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>練習</title>
+</head>
+<body>
+    <h1>test</h1>
+    <img src="{{ asset('img/test1.jpeg') }}" alt="">
+</body>
+</html>
